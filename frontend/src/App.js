5a303b396f95043ab0fb90468a6233d71998ed10
Gaytri Dodiya <dodiyadsvv@gmail.com>
2023-09-23 18:40:05 +0530

    set file structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
-
+import ForgetPassword from './Screens/ForgetPasswordScreen';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import ResetPasswordScreen from './Screens/ResetPasswordScreen';
+import { Nav, NavDropdown, Navbar } from 'react-bootstrap';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand href="#home">My Website</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#about">About</Nav.Link>
+            <Nav.Link href="#services">Services</Nav.Link>
+            <Nav.Link href="#contact">Contact</Nav.Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action1">Action 1</NavDropdown.Item>
+              <NavDropdown.Item href="#action2">Action 2</NavDropdown.Item>
+              <NavDropdown.Item href="#action3">Action 3</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/ForgetPassword' element={<ForgetPassword />} />
+          <Route path='/resetPassword' element={<ResetPasswordScreen />} />
+          <Route />
+        </Routes>
+      </BrowserRouter>
+      <ForgetPassword />
     </div>
   );
 }
