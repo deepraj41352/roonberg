347c4d3534c1b256cdef2c803f9c75157ac0c33f
Dipak <deepraj41352@gmail.com>
2023-09-23 14:50:51 +0530

    setup for roonberg

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,12 @@ import express from 'express';
 
 const app = express();
 
+const _dirname = path.resolve();
+app.use(express.static(path.join(_dirname, 'frontend/build')));
+app.get('*', (req, res) =>
+  res.sendFile(path.join(_dirname, 'frontend/build/index.htlm'))
+);
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`server is running on port : ${port}`);
