bc4c8434b0c0eed15689fa3b044bc3a5284c3240
unknown <priynsujn01@gmail.comgit config --global user.name>
2023-09-25 16:09:43 +0530

    Add Registration Screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import SignUpForm from "./Screens/SignUpScreen";
+import RegistrationForm from "./Screens/RegistrationScreen";
 function App() {
   return (
     <div className="App">
       <SignUpForm />
+      <RegistrationForm />
     </div>
   );
 }
--- frontend/src/Screens/RegistrationScreen.js
+++ frontend/src/Screens/RegistrationScreen.js
@@ -3,19 +3,27 @@ import Form from "react-bootstrap/Form";
 import { Container, Row, Col, Card } from "react-bootstrap/";
 import { Link } from "react-router-dom";
 
-function SignUpForm() {
+function RegistrationForm() {
   return (
     <Container className="Sign-up-container d-flex  flex-column justify-content-center align-items-center">
       <div className="Sign-up-container-inner px-4 py-3">
         <Row className="mb-3">
           <Col>
-            <h4>Login</h4>
+            <h4>Registration</h4>
           </Col>
         </Row>
         <Row>
           <Col>
             <Card>
               <Form className="p-4 formWidth ">
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                  <Form.Label className="mb-1 input-box">First Name</Form.Label>
+                  <Form.Control type="text" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                  <Form.Label className="mb-1 input-box">Last Name</Form.Label>
+                  <Form.Control type="text" />
+                </Form.Group>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                   <Form.Label className="mb-1 input-box">
                     Email address
@@ -26,12 +34,10 @@ function SignUpForm() {
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                   <Form.Label className="mb-1">Password</Form.Label>
                   <Form.Control type="password" />
-
-                  <Form.Check
-                    className="mt-3"
-                    type="checkbox"
-                    label="Remember me"
-                  />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                  <Form.Label className="mb-1">Confirm Password</Form.Label>
+                  <Form.Control type="password" />
                 </Form.Group>
                 <Button
                   className="w-100 py-1 globalbtnColor"
@@ -40,12 +46,6 @@ function SignUpForm() {
                 >
                   Submit
                 </Button>
-                <Form.Group className="my-3">
-                  {/* <Link to="/ForgetPassword">Forgot Password?</Link> */}
-                  <a className="Fpswd-Clogin" href="#">
-                    Forgot Password?
-                  </a>
-                </Form.Group>
               </Form>
             </Card>
           </Col>
@@ -55,4 +55,4 @@ function SignUpForm() {
   );
 }
 
-export default SignUpForm;
+export default RegistrationForm;
