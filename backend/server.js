b836a132c8311e042e45e7b5f44b46db2baebd87
Dipak <deepraj41352@gmail.com>
2023-09-25 15:17:51 +0530

    change swagger doc

--- backend/server.js
+++ backend/server.js
@@ -43,7 +43,7 @@ const options = {
 };
 
 const swaggerSpec = swaggerJSDoc(options);
-app.use('/api', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.use('/api/doc', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
