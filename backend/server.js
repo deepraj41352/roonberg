c5684d5bb638b99433417b2d0bc96cb36e3456c0
Dipak <deepraj41352@gmail.com>
2023-09-29 10:59:57 +0530

    add some api delete and update

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,73 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import path from 'path';
+import userRouter from './routers/userRouter.js';
+import swaggerJSDoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
+
+dotenv.config();
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+
+    autoIndex: true, //make this also true
+  })
+  .then(() => {
+    console.log('connected to db');
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
 
 const app = express();
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'RoonBerg',
+      version: '1.0.6',
+    },
+    contact: {
+      email: 'deepraj41352@gmail.com',
+    },
+    servers: [
+      {
+        url:
+          process.env.NODE_ENV !== 'production'
+            ? 'http://localhost:5000'
+            : 'https://roonberg.onrender.com',
+      },
+    ],
+    schemes: ['https', 'http'],
+  },
+  apis: ['./server.js', './routers/userRouter.js'],
+};
+
+const swaggerSpec = swaggerJSDoc(options);
+
+app.use('/api/doc', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/test', (req, res) => {
+  res.send('Welcome to Roonberg World');
+});
+
+app.use('/api/user', userRouter);
+
+const _dirname = path.resolve();
+app.use(express.static(path.join(_dirname, 'frontend/build')));
+app.get('*', (req, res) =>
+  res.sendFile(path.join(_dirname, 'frontend/build/index.htlm'))
+);
+
+app.use((err, req, res, next) => {
+  console.log(err.message);
+  res.status(500).send({ message: err.message });
+});
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
