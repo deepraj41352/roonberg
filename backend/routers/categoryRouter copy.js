3e030924263aede03abec9d4ae313c42f799e24d
Dipak <deepraj41352@gmail.com>
2023-10-02 11:07:25 +0530

    create add user api

--- backend/routers/categoryRouter copy.js
+++ backend/routers/categoryRouter copy.js
@@ -2,7 +2,9 @@ import express from 'express';
 import Category from '../Models/categoryModel.js';
 import expressAsyncHandler from 'express-async-handler';
 import { isAuth, isAdminOrSelf } from '../util.js';
-
+import multer from 'multer';
+import { uploadDoc } from './userRouter.js';
+const upload = multer();
 const categoryRouter = express.Router();
 
 categoryRouter.get(
@@ -22,8 +24,13 @@ categoryRouter.post(
   '/',
   isAuth,
   isAdminOrSelf,
+  upload.single('file'),
   expressAsyncHandler(async (req, res) => {
     try {
+      if (req.file) {
+        const profile_picture = await uploadDoc(req);
+        req.body.categoryImage = profile_picture;
+      }
       //const user = await User.find();
       const newcategory = new Category({
         categoryName: req.body.categoryName,
